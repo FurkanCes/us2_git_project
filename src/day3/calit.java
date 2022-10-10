f8e092c047b0a1357eda56d6c4e7790f92171e36
furkan <furkancesaretli@gmail.com>
2022-10-10 11:06:42 -0400

    calit day3 is created helloworld removed

---
+++ src/day3/calit.java
@@ -0,0 +1,11 @@
+package day3;
+
+public class calit {
+    public static void main(String[] args) {
+        int a = 0;
+        if (a< 5){
+            System.out.println("a is just a number");
+        }
+
+    }
+}
